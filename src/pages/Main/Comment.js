52a11567ae91342b6ecbe1f33c2280674a200076
Oh9yu <kog6148@gmail.com>
2022-11-27 19:30:39 +0900

    Copanent devide : first commit

--- src/pages/Main/Comment.js
+++ src/pages/Main/Comment.js
@@ -6,17 +6,44 @@ function CommentActive(props) {
   const [comment, setComment] = useState("");
   const [commentList, setCommentList] = useState([]);
   // const [commentListLi, setcommetListLi] = useState(commentList);
+
+  const [commentLike, setcommentLike] = useState(0);
+
+  const commentDelete = (e) => {
+    const commentEle = e.target.parentElement;
+    commentEle.remove();
+  };
+
+  const commentLikeCount = (e, props) => {
+    e.target.checked
+      ? setcommentLike(commentLike + 1)
+      : setcommentLike(commentLike - 1);
+  };
+
   const commentShow = commentList.map((e, i) => {
     // <CommentEle key={i} value={e}></CommentEle>;
-    return <li key={i}>{e}</li>;
+    return (
+      <li key={i}>
+        {/* 로그인 사용자 명 */}
+        {e}
+        {/* <button onClick={deleteBtn}>삭제</button> */}
+        {/* span value값은 데이터 받아서 */}
+        <input type="checkbox" onClick={commentLikeCount} />
+        {/* 서버에 저장한 */}
+        <span key={i}>{commentLike}</span>
+        <span> 좋아요</span>
+        <button onClick={commentDelete}>삭제</button>
+      </li>
+    );
   });
+
   //댓글입력
   const inputComment = (e) => {
     setComment(e.target.value);
     // console.log(comment);
   };
 
-  const addComment = (e) => {
+  const addComment = () => {
     setCommentList(commentList.concat(comment));
     setComment("");
   };
@@ -24,7 +51,10 @@ function CommentActive(props) {
   return (
     <div id="commentSection">
       <ul className="commentList">{commentShow}</ul>
-      <span id="commentUserInfo">user1</span>
+
+      <span id="commentUserInfo" value="user1">
+        {/* 서버에서 가져온 로그인 유저 이름 */}
+      </span>
       <input
         id="commentInput"
         type="text"
--- src/pages/Main/CommentLi.js
+++ src/pages/Main/CommentLi.js
@@ -1,7 +1,36 @@
-import React from "react";
+// import React, { useState } from "react";
 
-function CommentEle(props) {
-  return <li>{props.value}</li>;
-}
+// function CommentElement(props) {
+//   const [commentLike, setcommentLike] = useState(0);
 
-export default CommentEle;
+//   const commentDelete = (e) => {
+//     const commentEle = e.target.parentElement;
+//     commentEle.remove();
+//   };
+
+//   const commentLikeCount = (e) => {
+//     e.target.checked
+//       ? setcommentLike(commentLike + 1)
+//       : setcommentLike(commentLike - 1);
+//   };
+
+//   const commentArr = props.commentArr;
+//   const commentShow = commentArr.map((e, i) => {
+//     // <CommentEle key={i} value={e}></CommentEle>;
+//     return (
+//       <li key={i}>
+//         {/* 로그인 사용자 명 */}
+//         {e}
+//         {/* <button onClick={deleteBtn}>삭제</button> */}
+//         {/* span value값은 데이터 받아서 */}
+//         <input type="checkbox" onClick={commentLikeCount} />
+//         {/* 서버에 저장한 */}
+//         <span>{commentLike}</span>
+//         <span> 좋아요</span>
+//         <button onClick={commentDelete}>삭제</button>
+//       </li>
+//     );
+//   });
+// }
+
+// export default CommentElement;
